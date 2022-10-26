5b0b66bebffde369d02e9d4b6654381270f8ec05
jamesyoung0623 <jamesyoung0623.gmail.com>
2022-10-27 07:56:47 +0800

    10/27

---
+++ dataloaders/data_rgb.py
@@ -0,0 +1,19 @@
+import os
+from .dataset_rgb import DataLoaderTrain, DataLoaderVal , DataLoaderTest, DataLoader_NoisyData
+
+
+def get_training_data(rgb_dir):
+    assert os.path.exists(rgb_dir)
+    return DataLoaderTrain(rgb_dir, None)
+
+def get_validation_data(rgb_dir):
+    assert os.path.exists(rgb_dir)
+    return DataLoaderVal(rgb_dir, None)
+
+def get_test_data(rgb_dir):
+    assert os.path.exists(rgb_dir)
+    return DataLoaderTest(rgb_dir, None)
+
+def get_rgb_data(rgb_dir):
+    assert os.path.exists(rgb_dir)
+    return DataLoader_NoisyData(rgb_dir)
